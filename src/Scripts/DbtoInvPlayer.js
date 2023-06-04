cd9c8384150e5adbb1fc914d4d076a194e478bb2
JuanMaHern <ju4n.m4rtin.18@gmail.com>
2023-06-04 14:13:54 -0300

    DbtoInvPlayer fixed atribCalc

--- src/Scripts/DbtoInvPlayer.js
+++ src/Scripts/DbtoInvPlayer.js
@@ -1,16 +1,32 @@
 import itemDb from '../JSON/Items.json'
 
-export function DbtoInvPlayer (player) {
+const atribMultip = (item) => {
+    let auxItem = JSON.parse(JSON.stringify(item))
+    const multip = auxItem.rarity === 'Legendary' ?
+        2.5 : auxItem.rarity === 'Epic' ? 2 : auxItem.rarity === 'Rare' ? 1.5 : 1
+    if (auxItem.type === 'Weapon') {
+        auxItem.damage = Math.floor(auxItem.damage*multip)
+    }
+    if (auxItem.type === 'Armor') {
+        auxItem.defense = Math.floor(auxItem.defense*multip)
+    }
+    auxItem.price = Math.floor(auxItem.price*multip)
+    return auxItem
+}
+
+export function DbtoInvPlayer(player) {
     let auxPlayer = JSON.parse(JSON.stringify(player))
     let auxInv = []
     let auxEquip = []
-    for (let inv of player.character.inventori){
+    for (let inv of player.character.inventori) {
         let auxItem = itemDb.find(elem => elem.id === inv.id)
-        auxInv.push({...inv, ...auxItem, inv: 'I'})
+        let finalItem = atribMultip({ ...inv, ...auxItem, inv: 'I' })
+        auxInv.push(finalItem)
     }
-    for (let equip of player.character.equipment){
+    for (let equip of player.character.equipment) {
         let auxItem = itemDb.find(elem => elem.id === equip.id)
-        auxEquip.push({...equip, ...auxItem, inv: 'E'})
+        let finalItem = atribMultip({ ...equip, ...auxItem, inv: 'E' })
+        auxEquip.push(finalItem)
     }
     auxPlayer.character.inventori = auxInv
     auxPlayer.character.equipment = auxEquip
